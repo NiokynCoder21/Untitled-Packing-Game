a6382943333b7c012b4346c4cd35fab1a51f44d4
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-08-12 23:28:22 +0200

    Flip sprite and do not stop character immediatley added
    I adjusted the script to flip the sprite and not stop the player immediatly when input is not given, previously it made the player slide currently it feels good will adjsut
    Set up all food spawn points and re did scence
    Put all food into one folder for convience

--- Untitled Players vs Snake Project Files/Assets/Scripts/PlayerMovement.cs
+++ Untitled Players vs Snake Project Files/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Transform orientation;
     public float moveForce;
+    public SpriteRenderer spriteRenderer;
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -25,11 +26,14 @@ public class PlayerMovement : MonoBehaviour
         if (move.x > 0)
         {
             rb.AddForce(orientation.right * moveForce, ForceMode2D.Force);
+            spriteRenderer.flipX = true; //changes the sprites direction to the right
         }
 
         if (move.x < 0)
         {
             rb.AddForce(-orientation.right * moveForce, ForceMode2D.Force);
+            spriteRenderer.flipX = false; //changes the sprites direction to the left
+
         }
 
         if (move.y > 0)
@@ -40,6 +44,11 @@ public class PlayerMovement : MonoBehaviour
         if (move.y < 0)
         {
             rb.AddForce(-orientation.up * moveForce, ForceMode2D.Force);
+        }
+
+        if (move == Vector2.zero)
+        {
+            rb.velocity *= 0.9f; // Reduces the velocity gradually
         }
     }
 }
