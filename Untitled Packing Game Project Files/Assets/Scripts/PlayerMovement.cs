0847bfcf31d3ce5c741e981247f7c6f259c79755
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-09-10 18:33:40 +0200

    Stuff
    So added so it adds some speed when changing direction basically through code it kinda works

--- Untitled Packing Game Project Files/Assets/Scripts/PlayerMovement.cs
+++ Untitled Packing Game Project Files/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Vector2 move;
+    private Vector2 move;
     public Rigidbody2D rb;
     public Transform orientation;
     public float moveForce;
     public SpriteRenderer spriteRenderer;
+    public bool isRight = false;
+    public bool isMoving = false;
+    public float boostForce;
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -23,32 +26,53 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move()
     {
-        if (move.x > 0)
+        Vector2 normalizedMove = move.magnitude > 1 ? move.normalized : move;
+
+        if (normalizedMove.x > 0)
         {
             rb.AddForce(orientation.right * moveForce, ForceMode2D.Force);
             spriteRenderer.flipX = true; //changes the sprites direction to the right
+            isRight = true;
+            isMoving = true;
         }
 
-        if (move.x < 0)
+        if (normalizedMove.x < 0)
         {
             rb.AddForce(-orientation.right * moveForce, ForceMode2D.Force);
             spriteRenderer.flipX = false; //changes the sprites direction to the left
-
+            isRight = false;
+            isMoving = true;
         }
 
-        if (move.y > 0)
+        if (isMoving)
+        {
+            if (isRight == true)
+            {
+                print("Left faster");
+                rb.AddForce(orientation.right * boostForce * Time.deltaTime, ForceMode2D.Impulse);
+            }
+
+            if (isRight == false)
+            {
+                print("faster");
+                rb.AddForce(-orientation.right * boostForce * Time.deltaTime, ForceMode2D.Impulse);
+            }
+        }
+
+        if (normalizedMove.y > 0)
         {
             rb.AddForce(orientation.up * moveForce, ForceMode2D.Force);
         }
 
-        if (move.y < 0)
+        if (normalizedMove.y < 0)
         {
             rb.AddForce(-orientation.up * moveForce, ForceMode2D.Force);
         }
 
         if (move == Vector2.zero)
         {
-            rb.velocity *= 0.9f; // Reduces the velocity gradually
+            rb.velocity *= 0.1f; // Reduces the velocity gradually
+            isMoving = false;
         }
     }
 }
