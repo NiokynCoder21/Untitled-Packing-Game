c3bc0fc8273ca04d89ea3b515e39a44a9b728358
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-09-20 17:34:23 +0200

    restart and go back to menu works for all win and draw scences

--- Untitled Packing Game Project Files/Assets/Scripts/Restart.cs
+++ Untitled Packing Game Project Files/Assets/Scripts/Restart.cs
@@ -12,9 +12,51 @@ public class Restart : MonoBehaviour
         Retry();
     }
 
+    public void onStart(InputAction.CallbackContext context)
+    {
+        StartGame();
+    }
+
+    public void onQuit(InputAction.CallbackContext context)
+    {
+        QuitGame();
+    }
+
+    public void onContinue(InputAction.CallbackContext context)
+    {
+        ContinueGame();
+    }
+
+    public void onMainMenu(InputAction.CallbackContext context)
+    {
+        MainMenuGame();
+    }
+
     public void Retry()
     {
         Destroy(canvas);
         SceneManager.LoadScene("OutHouse", LoadSceneMode.Single); // Load Player 1 win scene
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("IntroScence", LoadSceneMode.Single); // Load Player 1 win scene
+    }
+
+    public void ContinueGame()
+    {
+        Destroy(canvas);
+        SceneManager.LoadScene("OutHouse", LoadSceneMode.Single); // Load Player 1 win scene
+    }
+
+    public void MainMenuGame()
+    {
+        Destroy(canvas);
+        SceneManager.LoadScene("StartScreen", LoadSceneMode.Single); // Load Player 1 win scene
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
