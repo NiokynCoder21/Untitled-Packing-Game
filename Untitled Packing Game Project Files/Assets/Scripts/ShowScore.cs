c3b623ad5fd9f1e8e8fb06087793611da9f6c582
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-09-20 14:19:41 +0200

    after timer ends it shows each players score

--- Untitled Packing Game Project Files/Assets/Scripts/ShowScore.cs
+++ Untitled Packing Game Project Files/Assets/Scripts/ShowScore.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ShowScore : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text player1ScoreText; // Text field to display Player 1's score
+    public TMP_Text player2ScoreText; // Text field to display Player 2's score
+
     void Start()
     {
-        
-    }
+        // Fetch the scores from PlayerPrefs
+        int player1Score = PlayerPrefs.GetInt("Player1Score", 0);
+        int player2Score = PlayerPrefs.GetInt("Player2Score", 0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update the UI text to display the scores
+        player1ScoreText.text = "Player 1 Score: " + player1Score.ToString();
+        player2ScoreText.text = "Player 2 Score: " + player2Score.ToString();
+
+        PlayerPrefs.DeleteKey("Player1Score");
+        PlayerPrefs.DeleteKey("Player2Score");
     }
 }
